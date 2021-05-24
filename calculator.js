e06c4d4aa69130f779fcb5aa6ef4d91d2f6f84b5
Bienn Viquiera <biennviquiera@gmail.com>
2021-05-24 11:55:40 -0700

    Added bodyparser and calculator function

--- calculator.js
+++ calculator.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const port = 3000;
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + "/index.html");
+})
+
+app.post('/', (req, res) => {
+  res.send("The result is " + (Number(req.body.num1) + Number(req.body.num2)));
 })
 
 app.listen(port, () => {
