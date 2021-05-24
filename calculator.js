ba881e48e9329e18a562a3c3b96ffc36b5b13d99
Bienn Viquiera <biennviquiera@gmail.com>
2021-05-24 12:24:14 -0700

    implemented bmi calculator form and form post response

--- calculator.js
+++ calculator.js
@@ -15,6 +15,17 @@ app.post('/', (req, res) => {
   res.send("The result is " + (Number(req.body.num1) + Number(req.body.num2)));
 })
 
+app.get('/bmi', (req, res) => {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+})
+
+app.post('/bmi', (req, res) => {
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
+  var bmi = weight / (height * height);
+  res.send("Your BMI is " + bmi);
+})
+
 app.listen(port, () => {
   console.log("Server started on port " + port);
 })
